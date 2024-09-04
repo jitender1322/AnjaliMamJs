137ad1efe48f85cdb4a8e7406a9f78e53356eba8
jitender1322 <jitendersarswat6@gmail.com>
2024-09-04 14:07:00 +0530

    practice

--- ReactJsTraning/HomeWork/FirstPractice/src/App.jsx
+++ ReactJsTraning/HomeWork/FirstPractice/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Task from './CustomCreation/TaskManager/Task'
+import ApiCard from './CustomCreation/ApiWithUseEffect/ApiCard'
+import ApiCard2 from './CustomCreation/ApiWithUseEffect/ApiCard2'
+import ApiCard4 from './CustomCreation/ApiWithUseEffect/ApiCard4'
 export default function App() {
   return (
     <div>
-
+      {/* <Task /> */}
+      <ApiCard4 />
     </div>
   )
 }
